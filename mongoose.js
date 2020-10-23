b9575ed0719a1d6ce2bafba220b7df85f9b91e57
Mike Cho <elegantspanishguitar@gmail.com>
2020-10-23 07:49:29 -0700

    changed mongoose to heroku

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/client-intake', {
+const local = 'mongodb://127.0.0.1:27017/client-intake'
+const heroku = process.env.MONGODB_URI; 
+mongoose.connect(heroku, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
